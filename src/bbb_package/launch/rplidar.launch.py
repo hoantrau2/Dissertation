77679654853a90a6801dc11e22116d7ecb07755e
hoantrau2 <duongngochoan852002@gmail.com>
2023-12-30 06:41:37 +0100

    aa

--- src/bbb_package/launch/rplidar.launch.py
+++ src/bbb_package/launch/rplidar.launch.py
@@ -3,7 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from launch_ros.actions import Node
+from launch_ros.actions import Node
 import launch_ros.actions 
 
 def generate_launch_description():
