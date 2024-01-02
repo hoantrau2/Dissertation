ce026a603c9c1d7def989fc13993949dcb55fcf8
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2024-01-02 10:29:10 +0700

    add mapping picture

---
+++ Firmware/bbb_firmware/build/src/battery/CMakeFiles/bbb_battery.dir/home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj.d
@@ -0,0 +1,41 @@
+src/battery/CMakeFiles/bbb_battery.dir/home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj: \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/hardware_watchdog/include/hardware/watchdog.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /media/ngochoan/Intermediate_memory/mobile_robot/Dissertation/Firmware/bbb_firmware/build/generated/pico_base/pico/version.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /media/ngochoan/Intermediate_memory/mobile_robot/Dissertation/Firmware/bbb_firmware/build/generated/pico_base/pico/config_autogen.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/boards/include/boards/pico.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/watchdog.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/watchdog.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/psm.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/psm.h
---
+++ Firmware/bbb_firmware/build/src/scheduler/CMakeFiles/bbb_scheduler.dir/home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj.d
@@ -0,0 +1,26 @@
+src/scheduler/CMakeFiles/bbb_scheduler.dir/home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj: \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /media/ngochoan/Intermediate_memory/mobile_robot/Dissertation/Firmware/bbb_firmware/build/generated/pico_base/pico/version.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /media/ngochoan/Intermediate_memory/mobile_robot/Dissertation/Firmware/bbb_firmware/build/generated/pico_base/pico/config_autogen.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/boards/include/boards/pico.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/ngochoan/micro_ros_ws/src/pico-sdk/src/common/pico_base/include/pico/error.h
